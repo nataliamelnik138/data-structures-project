5a92bcc8c35ac3a6316ac3b548bbd68521009733
Natalia Melnik <nataliamelnik138@gmail.com>
2023-06-21 23:09:21 +0600

    Tests written

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+    def test_push(self):
+        self.stack.push("data1")
+        self.stack.push("data2")
+        self.stack.push("data3")
+        self.assertEqual(self.stack.top.data,"data3")
+        self.assertEqual(self.stack.top.next_node.data, "data2")
+        self.assertEqual(self.stack.top.next_node.next_node.data, "data1")
+        self.assertEqual(self.stack.top.next_node.next_node.next_node, None)
+
