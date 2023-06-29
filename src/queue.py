6ab1e04a822056d7f129e2b54e4e7d9c11eb8177
Natalia Melnik <nataliamelnik138@gmail.com>
2023-06-29 19:27:02 +0600

    Реализованы методы __init__, enqueue и __str__ класса Queue и тесты к ним

--- src/queue.py
+++ src/queue.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,24 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+
+    def __iter__(self):
+        """
+        Возвращает итератор для очереди
+
+        :return: итератор для очереди
+        """
+        current = self.head
+        while current is not None:
+            yield current.data
+            current = current.next_node
 
     def dequeue(self):
         """
@@ -35,4 +54,4 @@ class Queue:
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        return f"{' '.join([str(item) for item in self])}"
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,23 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+
+from src.queue import Queue
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.queue = Queue()
+
+    def test_enqueue(self):
+        self.queue.enqueue('data1')
+        self.queue.enqueue('data2')
+        self.queue.enqueue('data3')
+        self.assertEqual(self.queue.head.data, "data1")
+        self.assertEqual(self.queue.head.next_node.data, "data2")
+        self.assertEqual(self.queue.tail.data, "data3")
+        self.assertEqual(self.queue.tail.next_node, None)
+
+    def test_str(self):
+        self.queue.enqueue('data1')
+        self.queue.enqueue('data2')
+        self.queue.enqueue('data3')
+        assert str(self.queue) == "data1 data2 data3"
