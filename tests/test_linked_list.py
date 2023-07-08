e65a4dc9e3f6405070959fe74e71517fc465a59f
Natalia Melnik <nataliamelnik138@gmail.com>
2023-07-08 16:15:26 +0600

    Добавлены тесты для класса LinkedList в tests/test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.ll = LinkedList()
+
+    def test_insert_and_str(self):
+        self.ll.insert_beginning({'id': 1})
+        self.ll.insert_at_end({'id': 2})
+        self.ll.insert_at_end({'id': 3})
+        self.ll.insert_beginning({'id': 0})
+        assert str(self.ll) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
