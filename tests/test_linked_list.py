3fb0665351616ef78a7b9fb67d00c5a79517ea36
Natalia Melnik <nataliamelnik138@gmail.com>
2023-07-12 14:26:44 +0600

    Добавлены тесты для методов to_list() и get_data_by_id() класса LinkedList

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -13,3 +13,16 @@ class TestLinkedList(unittest.TestCase):
         self.ll.insert_at_end({'id': 3})
         self.ll.insert_beginning({'id': 0})
         assert str(self.ll) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+
+    def test_to_list(self):
+        self.ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        lst = self.ll.to_list()
+        assert lst == [{'id': 1, 'username': 'lazzy508509'}, {'id': 2, 'username': 'mik.roz'}]
+
+    def test_get_data_by_id(self):
+        self.ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        self.ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        user_data = self.ll.get_data_by_id(3)
+        assert user_data == {'id': 3, 'username': 'mosh_s'}
